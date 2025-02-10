cabb1732b8d24bb499d4af7f10405629bd593af4
vgsanjaykumar <143058056+vgsanjaykumar@users.noreply.github.com>
2025-02-10 22:02:43 +0530

    nav head

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -18,11 +18,11 @@ const Navbar = () => {
             className={`fixed top-0 left-0 w-full z-50 transition-all duration-300 ${
                 isScrolled ? "bg-yellow-300 bg-opacity-0 backdrop-blur-md text-white" : "bg-yellow-300"
             }`}
-            style={{ backgroundColor: isScrolled ? "rgba(255, 223, 88, 0.0)" : "rgb(253 224 71)" }}
+            style={{ backgroundColor: isScrolled ?  "rgb(253 224 71)":"rgba(255, 223, 88, 0.0)" }}
         >
             <div className="h-16 mx-[20px] max-w-[800px] flex items-center justify-between px-6 ">
                 <div className={`flex items-center ${isScrolled ? "text-black":"text-white"} font-bold text-2xl`}>
-                    <img src="assets/natarajan.jpg" alt="" className="w-10" />
+                    <img src="assets/natarajan.jpg" alt="" className="w-10 rounded-full h-10" />
                     <span>Natarajan and Co</span>
                 </div>
                 <div className={`${isScrolled ? "text-black":"text-white"} hidden sm:block`}>
