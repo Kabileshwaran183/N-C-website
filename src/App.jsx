8f975c2fcd625f22aea291b8f205ad4b31e96cbe
Kabileshwaran183 <Kabileshwaran183@gmail.com>
2025-02-10 15:46:41 +0530

    products section

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,16 @@ import About from "./components/About"
 import Contact from "./components/Contact"
 import Navbar from "./components/Navbar"
 import Product from "./components/Product"
+import JsonData from "./components/Constants/Product.json"
 
 function App() {
 
   return (
     <>
     <div className="w-full h-auto px-4">
-        <Navbar />
+        <Navbar data={JsonData.Header}/>
         <div className="max-w-screen-xl mx-auto">
-          <Product />
+          <Product data={JsonData.Features}/>
           <About />
           <Contact />
         </div>
