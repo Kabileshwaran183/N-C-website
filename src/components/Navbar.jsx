3f988f2f987b8f25e1b49b506385bee92e23011b
vgsanjaykumar <143058056+vgsanjaykumar@users.noreply.github.com>
2025-02-10 21:49:32 +0530

    nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
 
     useEffect(() => {
         const handleScroll = () => {
-            setIsScrolled(window.scrollY > 50);
+            setIsScrolled(window.scrollY > 650);
         };
 
         window.addEventListener("scroll", handleScroll);
@@ -16,9 +16,12 @@ const Navbar = () => {
         <nav
             data-aos="fade-down"
             className={`fixed top-0 left-0 w-full z-50 transition-all duration-300 ${
-                isScrolled ? "bg-yellow-300 bg-opacity-0 backdrop-blur-md" : "bg-yellow-300"
+                isScrolled ? "bg-yellow-300 bg-opacity-0 backdrop-blur-md text-white" : "bg-yellow-300"
             }`}
-            style={{ backgroundColor: isScrolled ? "rgba(255, 223, 88, 0.2)" : "rgb(253 224 71)" }} 
+            style={
+                { backgroundColor: isScrolled ?  "rgb(253 224 71)":"rgba(255, 223, 88, 0.0) "}
+                  
+            } 
         >
             <div className="h-16 mx-[20px] max-w-[800px] flex items-center justify-between px-6">
                 <div className="flex items-center text-black font-bold text-2xl">
