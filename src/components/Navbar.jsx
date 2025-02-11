49391b82b6f5dd61648db0e55aeba6bc6a42c6d2
vgsanjaykumar <143058056+vgsanjaykumar@users.noreply.github.com>
2025-02-11 10:46:47 +0530

    nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -20,13 +20,9 @@ const Navbar = () => {
             }`}
             style={{ backgroundColor: isScrolled ?  "rgb(255, 234, 0)":"rgba(255, 234, 0, 0.0)" }}
         >
-            <div className="h-16 mx-[15px] max-w-[800px] flex justify-between items-center w-full  px-3 ">
-                <div className={`flex items-center  justify-center ${isScrolled ? "text-black":"text-white"} font-bold text-2xl`}>
-                    <img src="assets/natarajan.jpg" alt="" className="w-10 rounded-full h-10" />
-                    <span >Natarajan and Co</span>
-                </div>
-                <div className={` ${isScrolled ? "text-black":"text-white"}  hidden sm:block  `}>
-                    <ul className=" header flex  items-center gap-6  font-bold text-xl py-4 mr-4 ">
+          
+                <div className={` ${isScrolled ? "text-black":"text-white"}  hidden sm:block header `}>
+                    <ul className=" flex  items-center gap-6  font-bold text-xl py-4 mr-4 ">
                         <li><a href="#" className={` ${isScrolled ?"hover:text-[white]":"hover:text-[#FFEA00]"}`}>Home</a></li>
                         <li><a href="#" className={` ${isScrolled ?"hover:text-[white]":"hover:text-[#FFEA00]"}`}>About </a></li>
                         <li><a href="#" className={` ${isScrolled ?"hover:text-[white]":"hover:text-[#FFEA00]"}`}>Products</a></li>
@@ -34,7 +30,7 @@ const Navbar = () => {
                         <li><a href="#" className={` ${isScrolled ?"hover:text-[white]":"hover:text-[#FFEA00]"}`}>Contacts</a></li>
                     </ul>
                 </div>
-            </div>
+          
         </nav>
         </section>
     );
