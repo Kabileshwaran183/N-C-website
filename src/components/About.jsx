e982400008ad870ab6c61765d91205e7bc40ae08
vgsanjaykumar <143058056+vgsanjaykumar@users.noreply.github.com>
2025-02-11 10:28:05 +0530

    about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,7 +4,11 @@ export const About = () => {
     return (
         <section className="top-container-1">
                     <div id="about" className="py-16 bg-gray-100">
+                         <div className=" Title-div ">
+                    <h2 className="text-3xl font-bold text-gray-800 hover:text-yellow-500 ">About us</h2>
+                </div>
             <div className="container mx-auto px-6 md:px-12">
+                
                 <div className="grid grid-cols-1 md:grid-cols-2 gap-10 items-center">
                 
                     <div className="flex justify-center">
@@ -15,7 +19,7 @@ export const About = () => {
                         />
                     </div>
                     <div>
-                        <h2 className="text-3xl font-bold text-gray-800 mb-4">About Us</h2>
+                        <h2 className="text-3xl font-bold text-gray-800 mb-4">Natarajan and co</h2>
                         <p className="text-gray-600 leading-relaxed">
                             Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla facere eveniet perferendis delectus deleniti, voluptatum laboriosam officiis. Quae voluptate ipsam voluptates provident aliquam! Tempore vitae ab accusamus velit explicabo sapiente.
                         </p>
