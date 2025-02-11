0695331d0df2be4e1068a53c65ecac226e863106
Kabileshwaran183 <Kabileshwaran183@gmail.com>
2025-02-11 12:11:52 +0530

    pre

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -2,37 +2,35 @@ import React from "react";
 
 export const Services = ({ data }) => {
   return (
-    <section className="top-container-1"> 
-         <div id="services" className=" serv-parent">
-      <div className=" mx-auto px-6 md:px-12">
+    <section className="top-container-1">
+        <div className=" mx-auto px-10 md:px-20">
 
-         <div className=" Title-div ">
-                    <h2 className="text-3xl font-bold txt-shadow text-gray-800 hover:text-yellow-500 ">Dealership</h2>
+          <div className=" Title-div ">
+            <h2 className="text-3xl font-bold txt-shadow text-gray-800 hover:text-yellow-500 ">Dealership</h2>
           </div>
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-10">
-          {data
-            ? data.map((d, index) => (
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-10">
+            {data
+              ? data.map((d, index) => (
                 <div
                   key={index}
-                  className="flex flex-col items-center p-8 bg-white rounded-2xl shadow-lg hover:shadow-2xl transition-all duration-300 group transform hover:-translate-y-2"
+                  className="flex flex-col items-center bg-slate-50 rounded-2xl shadow-lg hover:shadow-2xl transition-all duration-300 group transform hover:-translate-y-2"
                 >
 
                   <div className=" servies-img ">
-                    <img src={d.icon}  />
+                    <img src={d.icon} />
                   </div>
 
-                  <div className="text-center">
-                    <h3 className="text-2xl font-bold text-gray-800 mb-2 group-hover:text-amber-500">
+                  <div className="text-center p-5">
+                    <h3 className="text-2xl font-bold text-gray-800 mb-10 group-hover:text-amber-500">
                       {d.name}
                     </h3>
-                    <p className="text-gray-600 text-base leading-relaxed">{d.text}</p>
+                    <p className="text-gray-600 p text-sm ">{d.text}</p>
                   </div>
                 </div>
               ))
-            : <p className="text-lg text-gray-500">Loading...</p>}
+              : <p className="text-lg text-gray-500">Loading...</p>}
+          </div>
         </div>
-      </div>
-    </div>
     </section>
 
   );
